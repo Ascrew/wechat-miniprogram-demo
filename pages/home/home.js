50d982b325b242d1ff0ac2d3ffe8db0b318cb0bf
Ascrew <1311585620@qq.com>
2022-02-24 22:39:21 +0800

    继续练习

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,1 +1,24 @@
-Page({});
+const app = getApp();
+
+Page({
+  data: {
+    items: [],
+    inputValue: ''
+  },
+  inputHandler(event) {
+    this.setData({
+      inputValue: event.detail.value || ''
+    });
+  },
+  buttonHandler(event) {
+    const newItem = this.data.inputValue.trim();
+    if(!newItem) return;
+    const itemArr = [...this.data.items, newItem];
+    wx.setStorageSync('itesm', itemArr);
+    this.setData({ items: itemArr })
+  },
+  onLoad() {
+    const itemArr = wx.getStorageSync('items') || [];
+    this.setData({ items: itemArr })
+  }
+});
