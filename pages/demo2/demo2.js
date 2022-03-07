c23ed6a9b830f28a6d6adbadb3faa99dfd73c842
Ascrew <1311585620@qq.com>
2022-03-07 16:51:23 +0800

    添加防抖

--- pages/demo2/demo2.js
+++ pages/demo2/demo2.js
@@ -1,6 +1,28 @@
 Page({
   data: {
-
+    timer: 0,
+    result: []
+  },
+  handleInput(e) {
+    clearTimeout(this.data.timer);
+    this.data.timer = setTimeout(() => {
+      wx.showLoading({
+        title: '加载中',
+        mask: true,
+      });
+      let inputValue = e.detail.value;
+      setTimeout(() => {
+        this.setData({
+          result: [
+            {
+              content: '一段内容',
+              date: '2022-02-01'
+            }
+          ]
+        })
+        wx.hideLoading();
+      }, 0)
+    }, 1500)
   },
   onLoad: function (options) {
 
